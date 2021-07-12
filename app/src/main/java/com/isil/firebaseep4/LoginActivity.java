4afa50bbd9e0bf223d6327d293069464148a7f41
Nextiva Challenge Resolver <17993682+Milumon@users.noreply.github.com>
2021-07-12 03:31:30 -0500

    boiler plate + login

---
+++ app/src/main/java/com/isil/firebaseep4/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.isil.firebaseep4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+
+}
